33c478947166815a5ee2bdff833b352229cefd0a
Emmanuel Bapama <bapamaemma10@gmail.com>
2026-04-06 09:12:14 +0000

    okay sure

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
 
 import 'features/home/homepage.dart';
 import 'core/services/cart_service.dart';
 
 void main() {
-  // Suppress error overlays - replace red error screens with empty container
-  ErrorWidget.builder = (FlutterErrorDetails details) {
-    return Container(
-      color: Colors.white,
-      child: const SizedBox.shrink(),
-    );
-  };
-  
-  // Suppress error overlays in release mode
-  FlutterError.onError = (FlutterErrorDetails details) {
-    if (kDebugMode) {
-      FlutterError.presentError(details);
-    }
-    // In release mode, errors are silently logged
-  };
-  
   runApp(const MyApp());
 }
 
